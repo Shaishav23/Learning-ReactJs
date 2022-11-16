979b97172271c439f40d49d03641e0ccc5cb41a0
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2022-11-15 21:29:21 -0500

    Routes
    Created routes using react router dom dependency in react.
    Mentioned all the routed in App.js file

--- electricity-app/src/components/Base.js
+++ electricity-app/src/components/Base.js
@@ -4,7 +4,9 @@ const Base=({title="Welcome to our website",children})=>{
     return(
         <div className="container-fluid">
             <h1>This is header</h1>
+
                 {children}
+                
             <h1> This is footer</h1>
         </div>
     )
---
+++ electricity-app/src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <h1>this is signup page</h1>
+        <p>welcome to signup page</p>
+    </div>
+  )
+}
+
+export default Signup
