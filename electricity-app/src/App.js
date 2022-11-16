832f9ca9d83608af845e64643993fa590127c413
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2022-11-15 20:42:26 -0500

    updated reactstrap

---
+++ electricity-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my homepage</h1>
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
